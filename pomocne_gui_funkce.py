d74da4d2ae8b9d44889ab623865ea0dc60b8765f
Prokop Mikulasek <prokop12@volny.cz>
2022-10-12 02:32:35 +0200

    Prvn žalostné základy GUI. Spíš jen nefunkční kostra po pozdější pokračování.

---
+++ pomocne_gui_funkce.py
@@ -0,0 +1,7 @@
+def vyhledej_v_databazi_osob():
+    '''Posle do backendu text od uzivatele a vrati vsechny vysledky'''
+    raise NotImplementedError()
+
+def vyhledej_v_databazi_klicu():
+    '''Posle do backendu text od uzivatele a vrati vsechny vysledky'''
+    raise NotImplementedError()
