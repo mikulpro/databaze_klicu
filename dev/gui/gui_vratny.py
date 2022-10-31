39ceccda053b2675fbfb85f0885a1bc3e13f73ac
Prokop Mikulasek <prokop12@volny.cz>
2022-10-31 23:34:27 +0100

    New login screen visage

--- dev/gui/gui_vratny.py
+++ dev/gui/gui_vratny.py
@@ -1,32 +1,51 @@
-from tkinter import Grid
-# from ...db_interface import Db as db
-from pomocne_gui_funkce import *
+# from tkinter import Grid
+# # from ...db_interface import Db as db
+# from pomocne_gui_funkce import *
 
-from kivy.app import App
-from kivy.uix.widget import Widget
-# from kivy.properties import NumericProperty, ReferenceListProperty
-from kivy.vector import Vector
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.image import Image
-from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
 from kivy.core.window import Window
 from kivy.lang import Builder
+from kivy.config import Config
 from kivymd.app import MDApp
 
-class LoginWindow(GridLayout):
-    pass
+colors = {
+    "Cyan": {
+        "200": "#005963",
+        "500": "#005963",
+        "700": "#005963",
+    },
+    "Gray": {
+        "200": "#ededed",
+        "500": "#ededed",
+        "700": "#ededed",
+    },
+    "Red": {
+        "200": "#ff0000",
+        "500": "#ff0000",
+        "A700": "#ff0000",
+    },
+    "Light": {
+        "StatusBar": "E0E0E0",
+        "AppBar": "#202020",
+        "Background": "#ededed",
+        "CardsDialogs": "#FFFFFF",
+        "FlatButtonDown": "#CCCCCC",
+    },
+}
 
 class LoginApp(MDApp):
     def build(self):
-        self.title = "Přihlášení se"
-        return LoginWindow()
+        self.theme_cls.colors = colors
+        self.theme_cls.primary_palette = "Gray"
+        self.theme_cls.accent_palette = "Cyan"
+        self.theme_cls.theme_style = "Light"
+        return Builder.load_file('login.kv')
 
 
 if __name__ == "__main__":
-    Window.clearcolor = (1, 1, 1, 1)
+    
     Window.size = (1080, 720)
+    Config.set('graphics', 'width', '1080')
+    Config.set('graphics', 'height', '720')
 
     login = LoginApp()
     login.run()
