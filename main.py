ea09eb06a8e1ecfb6996b89d71519d28dd8bc9ec
Prokop Mikulasek <prokop12@volny.cz>
2022-11-13 06:44:54 +0100

    Připojena nová databáze ke gui

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from dev.gui.gui_vratny import VratnyApp
 from dev.sqlite.db_interface import Db
 
-database = Db("sqlite:///db_old.sqlite")
+database = Db("sqlite:///dev/sqlite/db.sqlite")
 app = VratnyApp(database_object=database)
 app.run()
